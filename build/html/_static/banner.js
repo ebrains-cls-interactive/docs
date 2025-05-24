495ad21721e2a152c05e8d6684b327939b3f1555
Luca Leonardo Bologna <lucaleonardo.bologna@gmail.com>
2025-05-25 01:45:00 +0200

    deleted files

--- build/html/_static/banner.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    var src = $(".wy-side-nav-search").children("a").children("img")[0].src
-    $(".wy-side-nav-search").children("a").children("img").remove()
-    $(".wy-side-nav-search").prepend(' \
-        <a href="https://ebrains-cls-interactive.github.io/" style="margin: 0; display: block"> \
-            <img style="width: 100%; height: auto; border-radius: 0; margin: 0" src="' + src + '" alt="Logo"> \
-        </a>')
-})
